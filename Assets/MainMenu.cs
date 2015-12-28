f20488ce4a51a9f9225c611cd380f57f91d57567
Taylor may <spam@catalysetech.com>
2015-12-27 23:13:02 -0700

    End of day changes

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainMenu : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+namespace CoreSys
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public PrefabList prefabs;
+    }
 }
