6f0be5c8abeeb33a473ae78c175ebc512746f9ad
Taylor may <spam@catalysetech.com>
2015-12-29 14:04:25 -0700

    Added splash screen and image

---
+++ Assets/StartSplash.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace CoreSys.SplashScreen
+{
+    /// <summary>
+    /// Meant to handle splash screen fade
+    /// </summary>
+    public class StartSplash : MonoBehaviour
+    {
+        public Text t1, t2, t3;
+        public Image i1;
+
+        public void Start()
+        {
+            t1.color = new Color(t1.color.r, t1.color.g, t1.color.b, 50);
+            t2.color = new Color(t2.color.r, t2.color.g, t2.color.b, 50);
+            t3.color = new Color(t3.color.r, t3.color.g, t3.color.b, 50);
+            i1.color = new Color(i1.color.r, i1.color.g, i1.color.b, 50);
+        }
+
+        public void Update()
+        {
+            float newAlpha = Mathf.Lerp(t1.color.a, 0, 2 * Time.deltaTime);
+            t1.color = new Color(t1.color.r, t1.color.g, t1.color.b, newAlpha);
+            newAlpha = Mathf.Lerp(t2.color.a, 0, 2 * Time.deltaTime);
+            t2.color = new Color(t2.color.r, t2.color.g, t2.color.b, newAlpha);
+            newAlpha = Mathf.Lerp(t3.color.a, 0, 2 * Time.deltaTime);
+            t3.color = new Color(t3.color.r, t3.color.g, t3.color.b, newAlpha);
+            newAlpha = Mathf.Lerp(i1.color.a, 0, 2 * Time.deltaTime);
+            i1.color = new Color(i1.color.r, i1.color.g, i1.color.b, newAlpha);
+            if (t1.color.a < .01 && t2.color.a < .01 && t3.color.a < .01 && i1.color.a < .01)
+            {
+                t1.gameObject.SetActive(false);
+                t2.gameObject.SetActive(false);
+                t3.gameObject.SetActive(false);
+                i1.gameObject.SetActive(false);
+            }
+        }
+    }
+}
--- Assets/Window.cs
+++ Assets/Window.cs
@@ -8,18 +8,19 @@ namespace CoreSys
         private WindowManagement manager;
         public void Awake()
         {
+            //This script is always stored on the canvas.
             manager = transform.root.GetComponent<WindowManagement>();
+            if (manager) { }
+            else
+                Debug.Log("WindowManager Not Found!");
         }
         public void OnEnable()
         {
             manager.WindowActivated(this);
         }
-        public void OnDisable()
-        {
-            manager.WindowDeactivated(this);
-        }
         public void CloseWindow()
         {
+            manager.WindowDeactivated(this);
             Destroy(this.transform.gameObject);
         }
         public void MoveWindow()
