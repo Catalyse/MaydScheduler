14a598a2b045a0a21e70a3a7a45128849f264314
Taylor may <spam@catalysetech.com>
2016-01-04 20:02:44 -0700

    Small namespace changes

--- Assets/EmployeeManagement/SchedulingAlgorithm.cs
+++ Assets/EmployeeManagement/SchedulingAlgorithm.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using CoreSys.Employees;
 
 namespace CoreSys
 {
     public static class SchedulingAlgorithm
     {
-        public static void GenerateSchedule()
+        public static void GenerateSchedule(Week week, List<Employee> empList)
         {
 
         }
--- Assets/System/Week.cs
+++ Assets/System/Week.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace CoreSys.Types
+namespace CoreSys
 {
     public class Week
     {
