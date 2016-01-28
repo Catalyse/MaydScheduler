d01c3fed2de866998e3a3a1e8875a8a8ca58c1ac
Catalyse <spam@catalysetech.com>
2016-01-28 11:25:50 -0700

    Added constructors to Shift type and Datetime

--- Assets/System/Types/Shift.cs
+++ Assets/System/Types/Shift.cs
@@ -4,9 +4,20 @@ using CoreSys.Employees;
 
 namespace CoreSys
 {
-    public class Shift
+    public class Shift//This should be expanded to hold more data
     {
         public Employee employee;
         public int startShift, endShift;
+        public DateTime date;
+        
+        public Shift() { }
+        
+        public Shift(Employee emp, int start, int end, DateTime d)//may need to float these or something to handle non hourly incremental shifts 
+        {
+            employee = emp;
+            startShift = start;
+            endShift = end;
+            DateTime date = d;
+        }
     }
 }
