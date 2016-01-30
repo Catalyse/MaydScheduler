d1d07319c0dc0d72437d786c47d242346f2a1cd9
Taylor may <spam@catalysetech.com>
2016-01-30 10:28:58 -0700

    Added new prefabs for: SchedulerWindow EmployeeScheduleBar Added new scripts for: SchedulerWindow EmployeeScheduleBar

---
+++ Assets/WindowScripts/EmployeeScheduleBar.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using CoreSys;
+
+namespace CoreSys.Windows
+{
+    public class EmployeeScheduleBar : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/WindowScripts/ScheduleWindow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using CoreSys;
+
+namespace CoreSys.Windows
+{
+    public class ScheduleWindow : Window
+    {
+        public PrefabList prefabs;
+
+    }
+}
