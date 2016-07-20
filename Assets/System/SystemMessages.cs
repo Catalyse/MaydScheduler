a10b9785a63c85e47dca54a91d6470704d352a28
Taylor may <spam@catalysetech.com>
2016-07-20 12:40:02 -0600

    Started work on system messages, allowing you to see issues and messages without the debugger

---
+++ Assets/System/SystemMessages.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CoreSystem
+{
+	public class SystemMessages : MonoBehaviour
+	{
+		public List<string> messageList = new List<string>();
+		public void Update()
+		{
+
+		}
+	}
+}
